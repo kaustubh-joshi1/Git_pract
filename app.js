3c96aa892681a234e745cecea446b30b934295f1
Kaustubh Joshi <90269829+kaustubh-joshi1@users.noreply.github.com>
2024-01-13 11:37:35 +0530

    create 2 branch dev and cart

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-asdbnmdmnkdbjb
+var a = {x: 60};
+
+var b = Object.create(a);
+b.y = 90;
+console.log(b.__proto__.__proto__ === a.__proto__);
+
+var c = Object.create(b);
+c.z = 80;
+console.log(c.__proto__.__proto__.__proto__ === a.__proto__);
+
+console.log(c.z);
+console.log(c.x);
+console.log(c.y);
